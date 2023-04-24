22b0775e9e47d13f38b14e5e7ca3e29477a00a4a
Hlalu <otimnabaxa@gmail.com>
2023-04-24 11:45:56 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+main - print a random num 
+
+Return: 0
+
 int main(void)
 {
 	int n;
