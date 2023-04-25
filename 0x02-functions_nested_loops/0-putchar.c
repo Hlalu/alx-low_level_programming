6be10f6f22813420539efde142c63cf98fcd86fb
Hlalu <otimnabaxa@gmail.com>
2023-04-25 08:20:15 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0
  */
-
 int main(void)
 
 {
@@ -17,5 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 } 	
